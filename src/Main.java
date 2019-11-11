5a7321f8cfcb45e142e7d9dc0c46416811c9a458
William-Barry-Aix <wiwi.barry@hotmail.fr>
2019-11-11 21:48:06 +0100

    Ajout d'une interface

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import Graphe.Sommet;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -16,32 +17,43 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        File file = new File(args[0]);
+
         try {
+            File file = new File("source.txt");
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
+            System.out.println("Chargement du fichier...");
             while ((st = br.readLine()) != null) {
                 st = st.replaceAll("[,?;.:/!()-]", " ");
                 for (String mot : st.toUpperCase().split(" ")) {
                     if (mot.length() != 0) {
-                        System.out.println(mot.trim());
                         Main.MOTS.add(mot.trim());
                     }
                 }
             }
+            // Ajout des sommets correspondants aux mots
+            Sommet racine = new Sommet();
+            for (String mot: Main.MOTS) {
+                racine.ajoutMulti(mot);
+            }
+            System.out.println("Fichier chargé !");
+            recherche(racine);
 
-
+        } catch (FileNotFoundException ex){
+            System.err.println("Un fichier \"source.txt\" doit se trouver dans le répertoire "+
+                    System.getProperty("user.dir"));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        System.out.println(Main.MOTS);
-        // Ajout des sommets correspondants aux mots
-        Sommet racine = new Sommet();
-        for (String mot: Main.MOTS) {
-            racine.ajoutMulti(mot);
+
+    }
+
+    public static void recherche(Sommet racine){
+        while (true){
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Entrez un mot à chercher :");
+            String mot = myObj.nextLine();  // Read user input
+            racine.recherche(mot);
         }
-        //System.out.println(racine.recherche("CARTABLEAU"));
-        racine.recherche("Théâtre");
     }
 }
